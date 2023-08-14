4acea99b511d65ba99ef5bfa6bc6ce06bcedbe82
Honalain <nyirinkwaya2000@gmail.com>
2023-08-14 23:04:01 +0200

    program will assign a random number to the variable n each time it is executed source code in order to print the last digit of the number stored in the variable n.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,19 +9,23 @@
 int main(void)
 {
 int n;
+int last_digit;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* your code goes there */
-int last_digit = n % 10;
-if (n < 6 && n != 0)
+last_digit = n % 10;
+
+if (last_digit > 5)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+printf("Last digit of %d is %d and is grater than 5\n", n, last_digit);
 }
-else if (n == 0)
+else if (last_digit == 0)
 {
-printf("Last digit of %d is %d and is 0", n, last_digit);
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
 else
-printf("Last number of %d is %d and is greater than 5\n", n, last_digit);
+{
+printf("Last number of %d is %d and less than 6 and not 0\n", n, last_digit);
+}
 return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - entry point of program
+ * Description: printing lower case alphabt
+ * Return: indicate termination of the program
+ */
+int main(void)
+{
+
+
+
+
+
+
+
+return (0)
+
+}
