f95eaefa13af81945b3fe4b7a65893de81085026
Honalain <nyirinkwaya2000@gmail.com>
2023-08-18 02:33:39 +0200

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - function to draw straight line in terminal
+ * @n: function parameter
+ * Return: nothing it return as it is void
+ */
+
+void print_line(int n)
+{
+if (n <= o)
+	_putchar('\n');
+else
+
+{
+	int k;
+
+	for (k = 1 ; i <= k ; k++)
+		_putchar('_');
+	_putchar('\n');
+}
+
+}
