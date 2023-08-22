e4736e54869f324b06136aa7f2e601a53a05cd2b
Honalain <nyirinkwaya2000@gmail.com>
2023-08-22 07:34:11 +0200

    test

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#inluce "main.h"
+/**
+ * int _strlen - function to return length of string
+ * @s: pointer variable
+ * Return: nothing
+ */
+int _strlen(char *s)
+{
+int i;
+int counter = 0;
+for (i = 0 ; s[i]) != ''\0 ; i++)
+/* strlen - leturn length of string*/
+	count++;
+
+return (count);
+}
