1b5129a7f9ad6f7fa2b00f5ccfe5eaa504e71c85
Honalain <nyirinkwaya2000@gmail.com>
2023-08-30 05:03:43 +0200

    advanced Task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - function thatsums two two of the diagonals
+ * @s: pointer parameter 1 points to pointer
+ * @to: pointer char parameter that points to pointer
+ * return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
