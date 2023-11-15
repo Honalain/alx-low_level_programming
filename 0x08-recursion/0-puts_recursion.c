ffb5699a1c933246d5eac9f40ae1bf9e2a2f7f24
root <root@Nyirinkwaya.localdomain>
2023-11-15 22:43:23 +0200

    This is recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * _puts_recursion - print string
+ * @s: function argument
+ * Return: Always 0;
+ */
+void _puts_recursion(char *s)
+
+{
+
+	if (*s != '\0')
+
+	{
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+
+	}
+
+	else
+	_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This another new beginning with more energy .md
