36a07956043ff8f3f9f0ef5a8bca9b8332447943
root <root@Nyirinkwaya.localdomain>
2023-11-17 23:41:35 +0200

    task 1 and 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - function that acept command line arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: zero
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for( i = 0 ; i <= argc ; i++)
+	{
+
+	printf("%s\n", argv[i]);
+	}
+	return (0);
+}
