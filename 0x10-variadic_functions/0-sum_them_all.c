e070d34de75922718fd65ecf747829d3102edfa7
root <root@Nyirinkwaya>
2023-11-30 22:05:44 +0200

    This is task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,20 +8,21 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
-	int sum = 0;
+	unsigned int sum = 0;
 
 	va_list args;
 
 	va_start(args, n);
 
 	if (n == 0)
-	return (0);
+		return (0);
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(args, const unsigned int);
+		sum += va_arg(args, unsigned int);
 
 	}
+
 	va_end(args);
 
 	return (sum);
