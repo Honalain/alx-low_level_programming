c7f78addf384c5fc0e2220dda8867595b7ba7c6a
root <root@Nyirinkwaya>
2023-12-08 00:53:06 +0200

    advanced task 1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * print_before_main - function that print statement before main is excecuted
+ *
+ * Return: 0
+ */
+void __attribute__((constructor)) print_before_main()
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+return (0);
+}
