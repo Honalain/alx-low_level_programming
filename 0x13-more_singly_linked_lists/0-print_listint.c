d659bd611816f63251b9876c7c392147127e7919
root <root@Nyirinkwaya>
2023-12-11 21:13:01 +0200

    This is task 0 done

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_listint - function that print content of the linked list
+ * @h: pointer to the head of the list
+ *
+ * Return: return number of the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodeCount = 0;
+
+	printf("Linked list contents:\n");
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		nodeCount++;
+		h = h->next;
+	}
+	return (nodeCount);
+
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is the README.md file for project of more linked list usually this file must contain the userguide of the program
