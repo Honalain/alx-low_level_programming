ecd1d1ece1ea8c0a0f49925ce3d6208c7c49a33c
root <root@Nyirinkwaya>
2023-12-18 23:49:34 +0200

    this task o for in put out put project

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,12 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	FILE *file = fopen(filename, "a");
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
 	{
 		return (-1);
 	}
@@ -25,8 +30,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 
 	fclose(file);
-	return (1);
+	return (-1);
 	}
 
-	return(0);
+	fclose(file);
+	return (1);
 }
